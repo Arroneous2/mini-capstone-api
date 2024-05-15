18cf04ef1c0d8c48d7405dd1e8eff167aadceec0
Arroneous2 <thumpinbassbeat@gmail.com>
2024-05-15 15:12:29 -0500

    destroy method for a product

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -14,4 +14,19 @@ class ProductsController < ApplicationController
     @product.save
     render template: "products/show"
   end
+  
+  def update
+    @product = Product.find(params[:id])
+    @product.name = params[:name]
+    @product.price = params[:price]
+    @product.image_url = params[:image_url]
+    @product.description = params[:description]
+    render template: "products/show"
+  end
+
+  def destroy
+    @product = Product.find(params[:id])
+    @product.destroy
+    render template: "products/show"
+  end
 end
