411e898706f08c31884f1284fd5e0b035ade97f3
Arroneous2 <thumpinbassbeat@gmail.com>
2024-05-22 12:33:39 -0500

    db:migrate images

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_05_22_164108) do
+ActiveRecord::Schema[7.1].define(version: 2024_05_22_173203) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "images", force: :cascade do |t|
+    t.string "url1"
+    t.string "url2"
+    t.string "url3"
+    t.string "url4"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "products", force: :cascade do |t|
     t.string "name"
