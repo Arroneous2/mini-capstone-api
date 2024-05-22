d99a140c20043d8522e7d09a2755f3a67fd0b8d0
Arroneous2 <thumpinbassbeat@gmail.com>
2024-05-22 13:02:58 -0500

    Linked Images with Products with a foreign key

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_05_22_174933) do
+ActiveRecord::Schema[7.1].define(version: 2024_05_22_175159) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -18,6 +18,8 @@ ActiveRecord::Schema[7.1].define(version: 2024_05_22_174933) do
     t.string "url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "product_id"
+    t.index ["product_id"], name: "index_images_on_product_id"
   end
 
   create_table "products", force: :cascade do |t|
@@ -39,5 +41,6 @@ ActiveRecord::Schema[7.1].define(version: 2024_05_22_174933) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "images", "products"
   add_foreign_key "products", "suppliers"
 end
