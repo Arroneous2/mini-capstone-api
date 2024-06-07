8f8392bc0b276641f2a968c5914e8cb00316d754
Arroneous2 <thumpinbassbeat@gmail.com>
2024-06-07 13:41:56 -0500

    Lots of new authentication

---
+++ app/controllers/orders_controller.rb
@@ -0,0 +1,29 @@
+class OrdersController < ApplicationController
+  def create
+    @product = Product.where(id: params[:product_id])[0]
+    @order = Order.new(
+      user_id: current_user.id,
+      product_id: params[:product_id],
+      quantity: params[:quantity],
+      subtotal: @product.price * params[:quantity].to_i ,
+      tax: @product.tax * params[:quantity].to_i,
+      total: @product.total * params[:quantity].to_i,
+    )
+    @order.save!
+    render :show
+  end
+
+  def index
+    @orders = Order.all
+    render :index
+  end
+
+  def show
+    @order = Order.find(params[:id])
+    if current_user.id == @order.user_id
+      render :show
+    else
+      render json: {error: "Nah ah ah, not your stuff"}
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,11 @@ Rails.application.routes.draw do
 
   post "/users" => "users#create"
   post "/sessions" => "sessions#create"
+
+
+  get "orders" => "orders#index"
+  post "/orders" => "orders#create"
+  get "/orders/:id" => "orders#show"
  
 
   # Defines the root path route ("/")
