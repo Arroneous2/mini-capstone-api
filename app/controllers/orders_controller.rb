0755ceb04cb27b44d85e916017384c996a4594cf
Arroneous2 <thumpinbassbeat@gmail.com>
2024-06-20 08:29:24 -0500

    update order create with proper carted_products data and setting purchases

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,17 +2,35 @@ class OrdersController < ApplicationController
   before_action :authenticate_user
 
   def create
-    @product = Product.where(id: params[:product_id])[0]
+    @user_carted_products = current_user.carted_products
+    carted_products_subtotal = 0
+    carted_products_tax = 0
+    carted_products_total = 0
+    @user_carted_products.each do |carted_product|
+      if carted_product.status == "carted"
+        carted_products_subtotal  += carted_product.product.price * carted_product.quantity
+        carted_products_tax += carted_product.product.tax * carted_product.quantity
+        carted_products_total += carted_product.product.total * carted_product.quantity
+        carted_product.update(status: "purchased")
+      end
+    end
     @order = Order.new(
       user_id: current_user.id,
-      product_id: params[:product_id],
-      quantity: params[:quantity],
-      subtotal: @product.price * params[:quantity].to_i ,
-      tax: @product.tax * params[:quantity].to_i,
-      total: @product.total * params[:quantity].to_i,
+      subtotal: carted_products_subtotal,
+      tax: carted_products_tax,
+      total:carted_products_total,
     )
-    @order.save!
-    render :show
+
+    if @order.save
+      @user_carted_products.each do |carted_product|
+        if carted_product.status == "carted"
+          carted_product.update(status: "purchased", order_id: @order.id)
+        end
+      end
+      render :show
+    else
+      render json: { errors: "could not save" }, status: :bad_request
+    end
   end
 
   def index
