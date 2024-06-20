277b1f92dbfc1f445b15ca31cf8c8b3b9ac6cee5
Arroneous2 <thumpinbassbeat@gmail.com>
2024-06-20 11:48:55 -0500

    simplify order create

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,17 +2,16 @@ class OrdersController < ApplicationController
   before_action :authenticate_user
 
   def create
-    @user_carted_products = current_user.carted_products
+    @carted_products = CartedProduct.where(user_id: current_user.id, status: "carted")
     carted_products_subtotal = 0
     carted_products_tax = 0
     carted_products_total = 0
-    @user_carted_products.each do |carted_product|
-      if carted_product.status == "carted"
-        carted_products_subtotal  += carted_product.product.price * carted_product.quantity
-        carted_products_tax += carted_product.product.tax * carted_product.quantity
-        carted_products_total += carted_product.product.total * carted_product.quantity
-        carted_product.update(status: "purchased")
-      end
+    @carted_products.each do |carted_product|
+      carted_products_subtotal  += carted_product.product.price * carted_product.quantity
+      carted_products_tax += carted_product.product.tax * carted_product.quantity
+      carted_products_total += carted_product.product.total * carted_product.quantity
+      carted_product.update(status: "purchased")
+      
     end
     @order = Order.new(
       user_id: current_user.id,
@@ -22,10 +21,8 @@ class OrdersController < ApplicationController
     )
 
     if @order.save
-      @user_carted_products.each do |carted_product|
-        if carted_product.status == "carted"
-          carted_product.update(status: "purchased", order_id: @order.id)
-        end
+      @carted_products.each do |carted_product|
+        carted_product.update(status: "purchased", order_id: @order.id)
       end
       render :show
     else
